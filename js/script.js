30fc7629bf42e03dd1af6704a2ae6b320814deaa
T.Yazadni <56879548+yazdani1994@users.noreply.github.com>
2022-04-22 17:13:02 +0430

    add dissappearwithparent

--- js/script.js
+++ js/script.js
@@ -9,8 +9,15 @@ function appear() {
     parent.style.display = 'block'
     section.style.filter = 'blur(10px)'
 }
-X.addEventListener('click', dissappear)
-function dissappear() {
+X.addEventListener('click', dissappearwithx)
+function dissappearwithx() {
     parent.style.display = 'none'
     section.style.filter = 'blur(0px)'
+}
+parent.addEventListener('click', dissappearwithparent)
+function dissappearwithparent(event) {
+    if(event.target.className == "modal-parent") {
+        parent.style.display = 'none'
+        section.style.filter = 'blur(0px)'
+    }
 }
